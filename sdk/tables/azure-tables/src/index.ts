de42bf188baf5249eec466128d965f42c654ae3c
Mahdi Varposhti <varp2010m@gmail.com>
2020-07-27 05:46:33 -0600

    Add odata helper class for escaping filter expressions

--- sdk/tables/azure-tables/src/index.ts
+++ sdk/tables/azure-tables/src/index.ts
@@ -5,6 +5,7 @@ export * from "./generatedModels";
 export * from "./models";
 export { EdmGuid } from "./EdmGuid";
 export { EdmInt64 } from "./EdmInt64";
+export { odata } from "./utils/odata";
 
 export { TableServiceClient } from "./TableServiceClient";
 export { TableClient } from "./TableClient";
